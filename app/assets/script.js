a26278865ae8f24cb2d121038cf4f627335d19c0
range-rules <evelinhelfrick@gmail.com>
2024-10-06 08:56:10 -0700

    display distance and sort by distance

--- app/assets/script.js
+++ app/assets/script.js
@@ -65,6 +65,26 @@ function loginComponent() {
 loginComponent();
 
 // gps
+
+// https://stackoverflow.com/questions/18883601/function-to-calculate-distance-between-two-coordinates
+function getDistanceFromLatLonInM(lat1, lon1, lat2, lon2) {
+  var R = 6371; // Radius of the earth in km
+  var dLat = deg2rad(lat2-lat1);  // deg2rad below
+  var dLon = deg2rad(lon2-lon1);
+  var a =
+    Math.sin(dLat/2) * Math.sin(dLat/2) +
+    Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) *
+    Math.sin(dLon/2) * Math.sin(dLon/2)
+    ;
+  var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+  var d = R * c; // Distance in km
+  return d * 1000;
+}
+
+function deg2rad(deg) {
+  return deg * (Math.PI/180)
+}
+
 function geoFindMe() {
     const trackPeriod = 1000; // ms
 
@@ -121,6 +141,18 @@ function updateProductsComponent(products) {
     productsComponent.id = elId;
     document.querySelector("body").appendChild(productsComponent);
 
+    // add distance info and sort by distance
+    for (let item of products) {
+        let distance = getDistanceFromLatLonInM(
+            myApp['lat'], myApp['lon'],
+            item.lat, item.lon);
+        distance = Math.floor(distance);
+
+        item['distance'] = distance;
+    }
+
+    products.sort((a, b) => a.distance - b.distance);
+
     let itemTag;
     for (let item of products) {
         //console.log(itemData);
@@ -128,6 +160,7 @@ function updateProductsComponent(products) {
         itemTag.innerHTML = `
 <h2>${item.title} - ${item.price}</h2>
 <div>Location: ${item.lat}, ${item.lon}</div>
+<div>Distance: ${item.distance} m </div>
 <button class="buttonBuy">Buy Now</button>
 `;
         productsComponent.appendChild(itemTag);
